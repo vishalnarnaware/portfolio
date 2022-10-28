72e4f716067e9355c5471d71bfa745837db67436
vishalnarnaware <vishalnarnaware@outlook.com>
2022-10-29 03:54:10 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2a1013e2cda17813c322.js"
+    "url": "webpack-runtime-5ff358c9d965793cb396.js"
   },
   {
     "url": "framework-4cf38abc6a75f5fe2b64.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d7c437835dc535da52060c2cac0b8985"
+    "revision": "ce182e19763596bef85ddaab93aa12d6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-920c6831a0875a27ac29.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "909d6a08b32494a582108f93d0e3df20"
+    "revision": "9ef6eae7d72cae8d254dd489f98f248e"
   },
   {
     "url": "polyfill-50fe2dda60dff157ce20.js"
